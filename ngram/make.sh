091b94ab9244b62620a1f82c1d8367e175d30e6a
gcmalloc <gcmalloc@gmail.com>
2015-03-24 16:49:54 +0100

    remove the classes after compilation and generation of the jar file

--- ngram/make.sh
+++ ngram/make.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 rm *.jar
+mkdir classes
 javac -classpath $(hadoop classpath) src/**/*.java -d classes
 pushd classes
 jar cf ../Ngram.jar **/*.class
 popd
-rm -R classes/**/*.class
+rm -R classes
