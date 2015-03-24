10f63dfd6dc1dd5230c1d663df477dfa1e34c0f1
Marc Schaer <marc110191@gmail.com>
2015-03-24 12:33:21 +0100

    Init TFIDF

---
+++ TFIDF/src/TfIdf.java
@@ -0,0 +1,42 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+
+/**
+ * TF-IDF functionality.
+ * This code is to find the relevance of words through years
+ * @author Marc Schaer
+ *
+ */
+public class TfIdf {
+	
+	/**
+	 * Number of reducers.
+	 */
+	private static final int NBOFREDUCERS = 25;
+
+	/**
+	 * Main function.
+	 * @param args standard args
+	 * @throws Exception standard Exceptions
+	 */
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "TF-IDF");
+		conf.set(Job.NUM_MAPS, "25");
+
+		job.setNumReduceTasks(TfIdf.NBOFREDUCERS);
+
+		job.setJarByClass(TfIdf.class);
+		
+		//TF Parts
+		
+		//IDF Parts
+		
+		// To avoid The _SUCCESS files being created in the mapreduce output folder.
+		job.getConfiguration().setBoolean("mapreduce.fileoutputcommitter.marksuccessfuljobs", false);
+		
+		int result = job.waitForCompletion(true) ? 0 : 1;
+
+		System.exit(result);
+	}
+}
