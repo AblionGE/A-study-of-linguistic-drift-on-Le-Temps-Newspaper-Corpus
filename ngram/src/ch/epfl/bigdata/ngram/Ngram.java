febe574da43721ad9718c8d42180003119290b01
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2015-03-24 18:06:08 +0100

    add combiner, set static number of reducers and add for loop to handle 1 to ngramSize in one pass

--- ngram/src/ch/epfl/bigdata/ngram/Ngram.java
+++ ngram/src/ch/epfl/bigdata/ngram/Ngram.java
@@ -5,8 +5,6 @@ import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -20,7 +18,6 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
-import java.util.StringTokenizer;
 
 /**
  * Test.
@@ -39,7 +36,7 @@ public class Ngram {
 		private static final IntWritable ONE = new IntWritable(1);
 		
 		private Text gram = new Text();
-		private int ngramSize = 1;
+		private int ngramSize = 2;
 		private String ngramSeparator = ",";
 		
 		private String concat(Collection<String> stringCollection) {
@@ -60,28 +57,50 @@ public class Ngram {
 			InterruptedException {
 			
 			String stringArticle = article.toString();
-
-			Deque<String> currentNgram = new ArrayDeque<>();
  			String[] splittedArticle = stringArticle.split("\\s+");
-			int counter = 0;
+ 			
+ 			for (int i = 1; i <= ngramSize; i++) {
+ 				Deque<String> currentNgram = new ArrayDeque<>();
+				int counter = 0;
+				
+				//article is too small
+				if (splittedArticle.length < i) {
+				    return;
+				}
+	
+				for (; counter < i - 1; counter++) {
+					 currentNgram.addLast(splittedArticle[counter]);
+				}
+	
+				String year = String.valueOf(key.get());
+	
+				for (; counter < splittedArticle.length; counter++) {
+				    	currentNgram.addLast(splittedArticle[counter]);
+					gram.set(year + "//" + concat(currentNgram));
+					context.write(gram, ONE);
+					currentNgram.removeFirst();
+				}
+ 			}
+		}
+	}
+	
+	/**.
+	 * Combiner for the ngrams.
+	 * @author gbrechbu
+	 *
+	 */
+	private static class NgramCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {		
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+				throws IOException, InterruptedException {
+			int sum = 0;
+			Iterator<IntWritable> valuesIt = values.iterator();
 			
-			//article is too small
-			if (splittedArticle.length < ngramSize) {
-			    return;
+			while (valuesIt.hasNext()) {
+				IntWritable value = (IntWritable) valuesIt.next();
+				sum += value.get();
 			}
-
-			for (; counter < ngramSize - 1; counter++) {
-				 currentNgram.addLast(splittedArticle[counter]);
-			}
-
-			String year = String.valueOf(key.get());
-
-			for (;counter < splittedArticle.length; counter++) {
-			    	currentNgram.addLast(splittedArticle[counter]);
-				gram.set(year + "//" + concat(currentNgram));
-				context.write(gram, ONE);
-				currentNgram.removeFirst();
-			}
+			context.write(key, new IntWritable(sum));
 		}
 	}
 	
@@ -112,7 +131,6 @@ public class Ngram {
 			String[] parts = key.toString().split("//", 2);
 			String year = parts[0];
 			String ngram = parts[1];
-			Path outPath = FileOutputFormat.getOutputPath(context);
 			mout.write("Output", ngram, new IntWritable(sum), year);
 		}
 		
@@ -139,13 +157,14 @@ public class Ngram {
 		String[] userArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 		Job job = Job.getInstance(conf, "Ngram");
 		job.setJarByClass(Ngram.class);
-		job.setNumReduceTasks(Integer.parseInt(userArgs[2]));
+		job.setNumReduceTasks(100);
 		
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
         
 		job.setMapperClass(NgramMapper.class);
 		job.setReducerClass(NgramReducer.class);
+		job.setCombinerClass(NgramCombiner.class);
 		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Integer.class);
