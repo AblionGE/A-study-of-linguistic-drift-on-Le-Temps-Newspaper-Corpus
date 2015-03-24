dbdd47cc9c9654d3f87439b2af75be667a6103d4
gcmalloc <gcmalloc@gmail.com>
2015-03-24 16:50:23 +0100

    correction on the run script

--- ngram/run.sh
+++ ngram/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 #usage: 
 # ./run.sh input_dir output_dir
-hadoop jar Ngram.jar ch.epfl.bigdata.ngram.Ngram '/projects/dh-shared' ngram 15
+hadoop jar Ngram.jar ch.epfl.bigdata.ngram.Ngram "$1" "$2" 15
