076ed17f0a5a52b6836f70f0aab3a663aeb664c9
Marc Schaer <marc110191@gmail.com>
2015-04-22 21:07:45 +0200

    Removed useless code

--- stats/p_of_w_per_year/ProbabilityOfAWordPerYear.scala
+++ stats/p_of_w_per_year/ProbabilityOfAWordPerYear.scala
@@ -15,14 +15,12 @@ object ProbabilityOfAWordPerYear {
     val yearMax = 1998
 
     // Read all files
-    //val YearOccurrencesFile = "hdfs:///projects/linguistic-shift/stats/" + nbOfGrams + "-grams-Tot*"
-    val YearOccurrencesFile = "/home/marc/temp/1-grams-TotOccurenceYear*"
+    val YearOccurrencesFile = "hdfs:///projects/linguistic-shift/stats/" + nbOfGrams + "-grams-Tot*"
     val YearOccurrences = sc.textFile(YearOccurrencesFile)
 
     def compute_one_year(year: Integer) : Int = {
         if (year <= yearMax) {
-            val wordsFile = "hdfs:///projects/linguistic-shift/cor_ngrams/" + nbOfGrams + "-grams/" + year +"*"
-            val wordsFile = "/home/marc/temp/temp/1997*"
+            val wordsFile = "hdfs:///projects/linguistic-shift/corrected_ngrams/" + nbOfGrams + "-grams/" + year +"*"
             val words = sc.textFile(wordsFile)
 
             val total_words = YearOccurrences.map(e => e.split('\t')).filter(e => e(0).toInt == year).map(e => e(1)).collect
@@ -30,7 +28,6 @@ object ProbabilityOfAWordPerYear {
             val results = words.map(e => e.split(", ")).map(e => e(0).split('\t')).map(e => e(0) -> e(1).toDouble/total_words(0).toDouble)
 
             results.saveAsTextFile("hdfs:///projects/linguistic-shift/stats/ProbabilityOfAWordPerYear/" + nbOfGrams + "-grams/"+year)
-            //results.saveAsTextFile("/home/marc/temp/stats_help/ProbabilityOfAWordPerYear/" + nbOfGrams + "-grams/"+year)
 
             compute_one_year(year+1)
         } else {
