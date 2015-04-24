b96cad14732345f7a918daa652566136d2fb76b3
Marc Schaer <marc110191@gmail.com>
2015-04-24 17:59:43 +0200

    No need to all year occurrences, computed directly in the code

--- stats/p_of_w_over_all_corpus/probability_of_a_word.scala
+++ stats/p_of_w_over_all_corpus/probability_of_a_word.scala
@@ -10,22 +10,32 @@ object ProbabilityOfAWordInAllCorpus {
   def main(args: Array[String]) {
     val sc = new SparkContext(new SparkConf().setAppName("ProbabilityOfAWordInAllCorpus"))
 
-    val nbOfGrams = "1"
+    if (args.size != 3) {
+        // the input format is important to parse the data because they are not the same if the input file
+        // was create with MapReduce or with Spark
+        println("Use with 3 args : nbOfGrams, input file, output file")
+        exit(1)
+    }
+
+    val nbOfGrams = args(0)
 
     // Read all files
     val YearOccurrencesFile = "hdfs:///projects/linguistic-shift/stats/" + nbOfGrams + "-grams-Tot*"
-    val wordsFile = "hdfs:///projects/linguistic-shift/corrected_ngrams/" + nbOfGrams + "-grams/*"
+    //val wordsFile = "hdfs:///projects/linguistic-shift/corrected_ngrams/" + nbOfGrams + "-grams/*"
+    val wordsFile = args(1)
 
     val YearOccurrences = sc.textFile(YearOccurrencesFile)
     val words = sc.textFile(wordsFile)
 
-    val total_words = YearOccurrences.map(e => e.split("\t")(1)).map(e => (1,e.toDouble)).groupBy(e => e._1).map(e => e._2.map(f => f._2).toList).map(e => e.sum).collect
+    val total_words = words.flatMap(e => e.split(", ").map(f => f.split('\t')).map(e => (1, e(1).toDouble))).reduceByKey(_+_).map(e => e._2).collect
+    //val total_words = YearOccurrences.map(e => e.split("\t")(1)).map(e => (1,e.toDouble)).groupBy(e => e._1).map(e => e._2.map(f => f._2).toList).map(e => e.sum).collect
 
     val occurrences_per_words = words.map(e => e.split("\t")).groupBy(e => e(0)).map(e => (e._1, (e._2.toArray.map(f => f(1).toDouble)).sum))
 
     val results = occurrences_per_words.map(e => (e._1, e._2/total_words(0)))
 
-    results.saveAsTextFile("hdfs:///projects/linguistic-shift/stats/ProbabilityOfAWordOverAllYears/" + nbOfGrams + "-grams")
+    //results.saveAsTextFile("hdfs:///projects/linguistic-shift/stats/ProbabilityOfAWordOverAllYears/" + nbOfGrams + "-grams")
+    results.saveAsTextFile(args(2))
 
     sc.stop()
   }
