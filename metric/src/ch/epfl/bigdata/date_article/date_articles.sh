14d222504018ec77f2381cb962ddf8137da0a13f
Marc Schaer <marc110191@gmail.com>
2015-05-03 20:04:15 +0200

    date_articles script adapted for distance1, cosine and chi-square

--- metric/src/ch/epfl/bigdata/date_article/date_articles.sh
+++ metric/src/ch/epfl/bigdata/date_article/date_articles.sh
@@ -8,10 +8,15 @@
 #       - The number of loop is how many times the metrics should be run on randomly selected articles
 # OUTPUT : This script writes in the file 'mean_error.txt' the mean error for each metric for the given year.
 #
+# IMPORTANT : It's necessary to have the different executables of the metrics in the repository
 
 EXPECTED_ARGS=3
 TEMPORARY_DIRECTORY="hdfs:///projects/linguistic-shift/temp"
+DISTANCE1_ERROR[$3]=0
 KL_ERROR[$3]=0
+COSINE_ERROR[$3]=0
+CHISQUARE_ERROR[$3]=0
+NB_OF_METRICS=4
 
 if [ "$#" -ne $EXPECTED_ARGS ] || [ "$1" -le 0 ]|| [ "$2" -lt 1840 ] || [ "$2" -gt 1995 ] || [ "$3" -le 0 ]; then
   echo "Use: ${0} nb_of_articles year(1840-1995) nb_of_loop(>0)"
@@ -19,8 +24,20 @@ if [ "$#" -ne $EXPECTED_ARGS ] || [ "$1" -le 0 ]|| [ "$2" -lt 1840 ] || [ "$2" -
 fi
 
 # Removed old result and errors files
+if [ -f err_Distance1 ]; then
+        rm err_Distance1
+fi
+
 if [ -f err_KL ]; then
         rm err_KL
+fi
+
+if [ -f err_Cosine ]; then
+        rm err_Cosine
+fi
+
+if [ -f err_ChiSquare ]; then
+        rm err_ChiSquare
 fi
 
 if [ -f err_choose_articles ]; then
@@ -28,26 +45,122 @@ if [ -f err_choose_articles ]; then
 fi
 
 # Rename previous results file to create a new one
-counter=0
-while [ -f mean_error.txt_$counter ]
-do
-        counter=$(($counter+1))
-done
-mv mean_error.txt mean_error.txt_$counter
+if [ -f mean_error.txt ]; then
+    counter=0
+    while [ -f mean_error.txt_$counter ]
+    do
+            counter=$(($counter+1))
+    done
+    mv mean_error.txt mean_error.txt_$counter
+fi
 
 for i in `seq "$3"`
 do
-        echo "Loop" $i
+        echo "Iteration nr." $i
+
+######################## SELECTING ARTICLES ##############################
         echo "Selecting articles..."
         if [ -f "select_articles/target/scala-2.10/selectarticle_2.10-1.0.jar" ]; then
                 # Create a subset of articles
-                spark-submit --class "SelectArticle" --master yarn-cluster --executor-memory 8g --num-executors 50 select_articles/target/scala-2.10/selectarticle_2.10-1.0.jar "$2" "$1" hdfs:///projects/linguistic-shift/corrected_nGramArticle/nGram/ $TEMPORARY_DIRECTORY/articles/$i/ 2>err_choose_articles
+                spark-submit --class "SelectArticle" --master yarn-cluster --executor-memory 8g --num-executors 50 select_articles/target/scala-2.10/selectarticle_2.10-1.0.jar "$2" "$1" hdfs:///projects/linguistic-shift/corrected_nGramArticle/nGram/ $TEMPORARY_DIRECTORY/articles/${i}/${2} 2>err_choose_articles
         else
                 echo "The compilated code of select_articles.scala should be in 'select_articles/target/scala-2.10/selectarticle_2.10-1.0.jar'"
                 echo "In select_articles directory, just execute the command 'sbt package'"
                 exit
         fi
 
+######################## DISTANCE1 ##############################
+        echo "Computing Distance1..."
+        if [ -d "distance1" ]; then
+            if [ -f "distance1/Distance1Articles.jar" ]; then
+                # Run Distance1
+                hadoop jar distance1/Distance1Articles.jar ch/epfl/bigdata/Distance1Articles $TEMPORARY_DIRECTORY/articles/$i/$2 /projects/linguistic-shift/corrected_ngrams/1-grams $TEMPORARY_DIRECTORY/Distance1/$i 2>err_Distance1
+            else
+                cd "distance1"
+                ./compile.sh
+                cd ..
+                hadoop jar distance1/Distance1Articles.jar ch/epfl/bigdata/Distance1Articles $TEMPORARY_DIRECTORY/articles/$i/$2 /projects/linguistic-shift/corrected_ngrams/1-grams $TEMPORARY_DIRECTORY/Distance1/$i 2>err_Distance1
+            fi
+        else
+            echo "No jar for Distance1Articles"
+            hadoop fs -rm -r $TEMPORARY_DIRECTORY
+            exit
+        fi
+        echo "Getting result of Distance1 and parsing it..."
+        # Get Result and create results.csv for Distance1
+        hadoop fs -get $TEMPORARY_DIRECTORY/Distance1/$i/ && cat $i/* > results_D1.csv
+        rm -r $i/
+
+        # Find the smallest distance and add it into an array for Distance1
+        RES=`(cat results_D1.csv | awk 'BEGIN {FS=","}{print $2 " " $3}' | awk 'BEGIN{a=2; b=0}{if ($2<0.0+a) {a=0.0+$2; b=$1}} END{print b}')`
+        echo "Real year is $2 and predicted year is $RES"
+        DISTANCE1_ERROR[$i]=$(($RES-$2))
+        if [ ${DISTANCE1_ERROR["$i"]} -lt 0 ]; then
+                DISTANCE1_ERROR[$i]=$(echo "${DISTANCE1_ERROR[$i]} * -1" | bc -l)
+        fi
+
+######################## COSINE ##############################
+        echo "Computing Cosine Metric..."
+        if [ -d "cosine" ]; then
+            if [ -f "cosine/CosineArticles.jar" ]; then
+                # Run Distance1
+                hadoop jar cosine/CosineArticles.jar ch/epfl/bigdata/CosineArticles $TEMPORARY_DIRECTORY/articles/$i/$2 /projects/linguistic-shift/corrected_ngrams/1-grams $TEMPORARY_DIRECTORY/Cosine/$i 2>err_Cosine
+            else
+                cd "cosine"
+                ./compile.sh
+                cd ..
+                hadoop jar cosine/CosineArticles.jar ch/epfl/bigdata/CosineArticles $TEMPORARY_DIRECTORY/articles/$i/$2 /projects/linguistic-shift/corrected_ngrams/1-grams $TEMPORARY_DIRECTORY/Cosine/$i 2>err_Cosine
+            fi
+        else
+            echo "No jar for CosineArticles"
+            hadoop fs -rm -r $TEMPORARY_DIRECTORY
+            exit
+        fi
+        echo "Getting result of CosineArticles and parsing it..."
+        # Get Result and create results.csv for Cosine
+        hadoop fs -get $TEMPORARY_DIRECTORY/Cosine/$i/ && cat $i/* > results_Cos.csv
+        rm -r $i/
+
+        # Find the smallest distance and add it into an array for Cosine
+        RES=`(cat results_Cos.csv | awk 'BEGIN {FS=","}{print $2 " " $3}' | awk 'BEGIN{a=2; b=0}{if ($2<0.0+a) {a=0.0+$2; b=$1}} END{print b}')`
+        echo "Real year is $2 and predicted year is $RES"
+        COSINE_ERROR[$i]=$(($RES-$2))
+        if [ ${COSINE_ERROR["$i"]} -lt 0 ]; then
+                COSINE_ERROR[$i]=$(echo "${COSINE_ERROR[$i]} * -1" | bc -l)
+        fi
+
+
+######################## CHISQUARE ##############################
+        echo "Computing ChiSquare..."
+        if [ -d "chi-square" ]; then
+            if [ -f "chi-square/ChiSquareArticles.jar" ]; then
+                # Run Distance1
+                hadoop jar chi-square/ChiSquareArticles.jar ch/epfl/bigdata/ChiSquareArticles $TEMPORARY_DIRECTORY/articles/$i/$2 /projects/linguistic-shift/corrected_ngrams/1-grams $TEMPORARY_DIRECTORY/ChiSquare/$i 2>err_ChiSquare
+            else
+                cd "chi-square"
+                ./compile.sh
+                cd ..
+                hadoop jar chi-square/ChiSquareArticles.jar ch/epfl/bigdata/ChiSquareArticles $TEMPORARY_DIRECTORY/articles/$i/$2 /projects/linguistic-shift/corrected_ngrams/1-grams $TEMPORARY_DIRECTORY/ChiSquare/$i 2>err_ChiSquare
+            fi
+        else
+            echo "No jar for ChiSquare"
+            hadoop fs -rm -r $TEMPORARY_DIRECTORY
+            exit
+        fi
+        echo "Getting result of ChiSquare and parsing it..."
+        # Get Result and create results.csv for ChiSquare
+        hadoop fs -get $TEMPORARY_DIRECTORY/ChiSquare/$i/ && cat $i/* > results_ChiSquare.csv
+        rm -r $i/
+
+        # Find the smallest distance and add it into an array for ChiSquare
+        RES=`(cat results_ChiSquare.csv | awk 'BEGIN {FS=","}{print $2 " " $3}' | awk 'BEGIN{a=2; b=0}{if ($2<0.0+a) {a=0.0+$2; b=$1}} END{print b}')`
+        echo "Real year is $2 and predicted year is $RES"
+        CHISQUARE_ERROR[$i]=$(($RES-$2))
+        if [ ${CHISQUARE_ERROR["$i"]} -lt 0 ]; then
+                CHISQUARE_ERROR[$i]=$(echo "${CHISQUARE_ERROR[$i]} * -1" | bc -l)
+        fi
+
+######################## KULLBACK-LEIBLER ##############################
         echo "Computing Kullback-Leibler Divergence..."
         if [ -f "kullback-leibler/target/scala-2.10/kullback-leibler_2.10-1.0.jar" ]; then
                 # Run KL Divergence
@@ -55,43 +168,51 @@ do
         else
                 echo "The compilated code of select_articles.scala should be in 'kullback-leibler/target/scala-2.10/kullback-leibler_2.10-1.0.jar'"
                 echo "In kullback-leibler directory, just execute the command 'sbt package'"
-                hadoop fs -rm -r $TEMPORARY_DIRECTORY/articles/$i/
+                hadoop fs -rm -r $TEMPORARY_DIRECTORY
                 exit
         fi
-
-        echo "Getting result and parsing it..."
-
-        # Get Result and create results.csv
-        hadoop fs -get $TEMPORARY_DIRECTORY/KL/$i/ && cat $i/* > results.csv
-
-        # Remove local directory
+        echo "Getting result of KL and parsing it..."
+        # Get Result and create results.csv for KL
+        hadoop fs -get $TEMPORARY_DIRECTORY/KL/$i/ && cat $i/* > results_KL.csv
         rm -r $i/
 
-        # Find the smallest distance and add it into an array
-        RES=`(cat results.csv | awk 'BEGIN {FS=","}{print $2 " " $3}' | awk 'BEGIN{a=2; b=0}{if ($2<0.0+a) {a=0.0+$2; b=$1}} END{print b}')`
+        # Find the smallest distance and add it into an array for KL
+        RES=`(cat results_KL.csv | awk 'BEGIN {FS=","}{print $2 " " $3}' | awk 'BEGIN{a=2; b=0}{if ($2<0.0+a) {a=0.0+$2; b=$1}} END{print b}')`
         echo "Real year is $2 and predicted year is $RES"
         KL_ERROR[$i]=$(($RES-$2))
         if [ ${KL_ERROR["$i"]} -lt 0 ]; then
-                KL_ERROR[$i]=$(($KL_ERROR[$i] * -1))
+                KL_ERROR[$i]=$(echo "${KL_ERROR[$i]} * -1" | bc -l)
         fi
 
         # Rename the file to keep it for manual verification of minimum and other things in case of failure
         # mv results.csv results.csv_$i
-        rm results.csv
 done
+rm results*.csv
 
 # Remove temp directory
 hadoop fs -rm -r $TEMPORARY_DIRECTORY
 
 # Computing the mean error
 echo "Computing the mean error..."
+D1_SUM=0
 KL_SUM=0
+COS_SUM=0
+CHI_SUM=0
 for j in `seq "$3"`
 do
         KL_SUM=$(($KL_SUM + ${KL_ERROR[$j]}))
+        D1_SUM=$(($D1_SUM + ${DISTANCE1_ERROR[$j]}))
+        COS_SUM=$(($COS_SUM + ${COSINE_ERROR[$j]}))
+        CHI_SUM=$(($CHI_SUM + ${CHISQUARE_ERROR[$j]}))
 done
 
 KL_MEAN=$(echo "${KL_SUM}/$3" | bc -l)
+D1_MEAN=$(echo "${D1_SUM}/$3" | bc -l)
+COS_MEAN=$(echo "${COS_SUM}/$3" | bc -l)
+CHI_MEAN=$(echo "${CHI_SUM}/$3" | bc -l)
 
-echo "Mean error for different metrics for $1 of articles in year $2 with $3 iterations" >> mean_error.txt
-echo "Mean error in KL : " $KL_MEAN >> mean_error.txt
+echo "Mean error for different metrics for $1 articles in year $2 with $3 iterations" >> mean_error.txt
+echo "Mean error with Kullback Leibler : " $KL_MEAN >> mean_error.txt
+echo "Mean error with Distance1 : " $D1_MEAN >> mean_error.txt
+echo "Mean error with Cosine : " $COS_MEAN >> mean_error.txt
+echo "Mean error with Chi-Square : " $CHI_MEAN >> mean_error.txt
